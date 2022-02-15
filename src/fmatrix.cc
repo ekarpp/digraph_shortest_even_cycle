13e6ad1a947f677e9197cfafd645151241577455
ekarpp <eetu.karppinen@aalto.fi>
2022-02-15 14:04:42 +0200

    add skeleton for fmatrix det

--- src/fmatrix.cc
+++ src/fmatrix.cc
@@ -46,3 +46,39 @@ EMatrix FMatrix::lift() const
 
     return EMatrix(this->n, lifted);
 }
+
+/* PA = LU, L lower triangular with ones on diagonal
+ * and U upper triangular. P permutation matrix as vector.
+   modifies the object it is called on to L and U in single matrix. */
+vector<int> FMatrix::lup()
+{
+    vector<int> P(1,1);
+
+    return P;
+}
+
+GF_element FMatrix::det() const
+{
+    FMatrix LU = this->copy();
+    vector<int> P = LU.lup();
+    GF_element det = global::F.one();
+    /* PA = LU => det(A) = det(P^T)det(L)det(U)
+     * in current implementation L has diagonal of one
+     * thus det(L) = 1. det(U) is the product of its diagonal
+     * elements. det(P)=det(P^T) is the sgn of P. */
+    for (int i = 0; i < this->n; i++)
+        det *= LU[i][i];
+    /* check sgn of P. write func in util. its used elsewhere also. */
+    return det;
+}
+
+FMatrix FMatrix::copy() const
+{
+    vector<vector<GF_element>> m(this->n, vector<GF_element>(this->n));
+
+    for (int row = 0; row < this->n; row++)
+        for (int col = 0; col < this->n; col++)
+            m[row][col] = this->m(row,col);
+
+    return FMatrix(this->n, m);
+}
--- src/fmatrix.hh
+++ src/fmatrix.hh
@@ -28,6 +28,18 @@ public:
 
     EMatrix lift() const;
 
+    /* returns a permutation matrix P and modifies the object called on
+     * such that P*obj = obj.lup(). where obj.lup() contains L on the lower
+     * triangle and U on the upper triangle such that P*obj = L*U. note that
+     * L has ones on diagonal, thus L and U can be stored in one matrix. */
+    std::vector<int> lup();
+
+    /* uses lup(). calls it on copy of the object it is on called on */
+    GF_element det() const;
+
+    /* returns a copy of this */
+    FMatrix copy() const;
+
     const Matrix<GF_element> &get_m() const { return this->m; }
 
     std::vector<GF_element> operator[](int i) const
