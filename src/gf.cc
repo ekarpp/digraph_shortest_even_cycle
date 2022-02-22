f5334b6cf149747ec649a90c1c17c4da755e63db
ekarpp <eetu.karppinen@aalto.fi>
2022-02-22 13:10:06 +0200

    fix overflow in gf mask

--- src/gf.cc
+++ src/gf.cc
@@ -13,7 +13,7 @@ void GF2n::init(const int n, const uint64_t mod)
 {
     this->n = n;
     this->mod = mod;
-    this->mask = (1 << this->n) - 1;
+    this->mask = (1ll << this->n) - 1;
 
     cout << "initialized GF(2^" << n << ") with modulus: ";
     for (int i = n; i >= 0; i--)
