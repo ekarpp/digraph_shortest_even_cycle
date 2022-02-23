22ed48ebe2779b9c19c37a31e2a055308bd0ce2f
ekarpp <eetu.karppinen@aalto.fi>
2022-02-23 09:01:54 +0200

    add complete graphs and generator. recreate c30 as c32

---
+++ graphs/k_gen.py
@@ -0,0 +1,13 @@
+def K(n):
+	l = [x for x in range(n)]
+	grph = ""
+	for i in range(n):
+		lfilt = [str(x) for x in l if x != i]
+		grph += f"{' '.join(lfilt)}\n"
+	with open(f"k{n}", "w") as f:
+		f.write(grph)
+
+import sys
+
+for arg in sys.argv[1:]:
+	K(int(arg))
