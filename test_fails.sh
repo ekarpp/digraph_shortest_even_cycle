895c3baa4a61cad30c9ac94566975c4c15ed095b
Karppinen Eetu <eetu.karppinen@aalto.fi>
2022-04-22 13:41:40 +0300

    improve geng failed test script

--- test_fails.sh
+++ test_fails.sh
@@ -2,11 +2,19 @@
 
 make digraph
 
+total=0
+fail=0
+
 for f in geng-fail/$1/*
 do
-	echo "algo"
-	./digraph16 -qf $f
-	echo "brute"
-	./digraph16 -qbf $f
-	echo
+	algo=$(./digraph16 -qf $f | grep -v seed)
+	brute=$(./digraph16 -qbf $f | grep -v seed)
+
+	if [ "$algo" != "$brute" ]; then
+		fail=$((fail+1))
+	fi
+
+	total=$((total+1))
 done
+
+echo "$fail/$total failed"
