3e1454370defb3cab5383f117881e0b30bf2c7db
ekarpp <eetu.karppinen@aalto.fi>
2022-04-30 17:15:13 +0300

    modify extension performance test

--- tests/perf/extension_perf.cc
+++ tests/perf/extension_perf.cc
@@ -24,11 +24,12 @@ int main(int argc, char **argv)
         cout << "-t $int for amount of tests" << endl;
         return 0;
     }
+
     uint64_t seed = time(nullptr);
 
     uint64_t t = 1;
     int opt;
-    while ((opt = getopt(argc, argv, "s:t:")) != -1)
+    while ((opt = getopt(argc, argv, "mifs:t:")) != -1)
     {
         switch (opt)
         {
@@ -45,16 +46,49 @@ int main(int argc, char **argv)
     global::randgen.init(seed);
     global::E.init();
 
-    Extension_element a = global::E.random();
-    Extension_element b = global::E.one();
-    chrono::steady_clock::time_point start =
-        chrono::steady_clock::now();
+    vector<uint64_2_t> a(t);
+    vector<uint64_2_t> b(t);
+    vector<uint64_2_t> c(t);
+
     for (uint64_t i = 0; i < t; i++)
-        b *= a;
-    chrono::steady_clock::time_point end =
-        chrono::steady_clock::now();
-    b.print();
-    cout << t << " multiplications in time: " <<
+    {
+        a[i] = {
+            global::randgen() & global::E.get_mask(),
+            global::randgen() & global::E.get_mask()
+        };
+
+        b[i] = {
+            global::randgen() & global::E.get_mask(),
+            global::randgen() & global::E.get_mask()
+        };
+    }
+
+    chrono::steady_clock::time_point start;
+    chrono::steady_clock::time_point end;
+
+    start = chrono::steady_clock::now();
+    for (uint64_t i = 0; i < t; i++)
+        c[i] = global::E.euclid_rem(global::E.ref_mul(a[i], b[i]));
+    end = chrono::steady_clock::now();
+    cout << t << " multiplications (euclid + ref) in time: " <<
+        (chrono::duration_cast<chrono::microseconds>(end - start).count()) /1000000.0 << " s" << endl;
+
+
+    start = chrono::steady_clock::now();
+    for (uint64_t i = 0; i < t; i++)
+        c[i] = global::E.intel_rem(global::E.fast_mul(a[i], b[i]));
+    end = chrono::steady_clock::now();
+
+    cout << t << " multiplications (intel + fast) in time: " <<
+        (chrono::duration_cast<chrono::microseconds>(end - start).count()) /1000000.0 << " s" << endl;
+
+
+    start = chrono::steady_clock::now();
+    for (uint64_t i = 0; i < t; i++)
+        c[i] = global::E.mont_mul(a[i], b[i]);
+    end = chrono::steady_clock::now();
+
+    cout << t << " multiplications (montgomery + fast) in time: " <<
         (chrono::duration_cast<chrono::microseconds>(end - start).count()) /1000000.0 << " s" << endl;
     return 0;
 }
