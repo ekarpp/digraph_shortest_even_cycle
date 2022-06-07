ec85c77a49091dfbff379d5b45e2025e33a894ec
ekarpp <eetu.karppinen@aalto.fi>
2022-06-07 22:40:46 +0300

    refactor util

--- src/util.hh
+++ src/util.hh
@@ -2,7 +2,8 @@
 #ifndef UTIL_H
 #define UTIL_H
 
-#include <random>
+#include <algorithm>
+#include <set>
 #include <bitset>
 #include <vector>
 
@@ -12,18 +13,114 @@
 
 namespace util
 {
-    int log2(uint64_t a);
+/*    inline int log2(uint64_t a)
+    {
+        return 63 - __builtin_clzl(a);
+    }*/
 
-    void direct_undirected(std::vector<std::vector<int>> &adj);
+    /* given an adjacency list for an undirected graph,
+     * directs it such that edges are made one way
+     * with direction chosen uniformly at random. */
+    inline void direct_undirected(std::vector<std::vector<int>> &adj)
+    {
+        for (int u = 0; u < (int) adj.size(); u++)
+        {
+            std::vector<int> nbors = adj[u];
+            for (int i = 0; i < (int) nbors.size(); i++)
+            {
+                int v = nbors[i];
+                if (u > v)
+                    continue;
+                /* rndom choose del */
+                int keep = u;
+                int del = v;
+                if (global::randgen() % 2)
+                {
+                    keep = v;
+                    del = u;
+                }
+                std::vector<int>::iterator pos = std::find(
+                    adj[del].begin(),
+                    adj[del].end(),
+                    keep
+                );
+                adj[del].erase(pos);
+            }
+        }
+        return;
+    }
 
-    std::vector<GF_element> distinct_elements(int n);
+    /* returns n distinct random elements from
+     * global::F. (use LSFR?) */
+    inline std::vector<GF_element> distinct_elements(int n)
+    {
+        std::vector<GF_element> vec(n);
+        std::set<uint64_t> have;
+        for (int i = 0; i < n; i++)
+        {
+            GF_element e = global::F.random();
+            while (have.count(e.get_repr()) == 1)
+                e = global::F.random();
+            vec[i] = e;
+            have.insert(e.get_repr());
+        }
+        return vec;
+    }
 
-    Polynomial poly_interpolation(
+    /* la grange interpolation with gamma and delta
+     * note that we are in characteristic 2 and thus
+     * - = +. done with the formula (3.3) here:
+     * https://doi.org/10.1137/S0036144502417715 */
+    inline Polynomial poly_interpolation(
         const std::vector<GF_element> &gamma,
         const std::vector<GF_element> &delta
-    );
+    )
+    {
+        // assert(gamma.size() == delta.size())
+        // assert(n > 2)
+        int n = gamma.size();
+        Polynomial interp(n - 1);
 
-    Extension_element tau(Extension_element sigma, Extension_element v);
+        /* weights*/
+        std::vector<GF_element> w(n, global::F.one());
+        for (int j = 1; j < n; j++)
+        {
+            for (int k = 0; k < j; k++)
+            {
+                w[k] *= gamma[k] + gamma[j];
+                w[j] *= gamma[k] + gamma[j];
+            }
+        }
+
+        for (int j = 0; j < n; j++)
+            w[j].inv_in_place();
+
+        /* main polynomial [ prod_{i} (x + gamma_i) ]*/
+        std::vector<GF_element> P(n+1, global::F.zero());
+        P[n] = global::F.one();
+        P[n-1] = gamma[0];
+        for (int i = 1; i < n; i++)
+        {
+            for (int j = n - i - 1; j < n - 1; j++)
+                P[j] += gamma[i] * P[j+1];
+            P[n - 1] += gamma[i];
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            Polynomial tmp(P);
+            tmp.div(gamma[i]);
+            tmp *= w[i] * delta[i];
+            interp += tmp;
+        }
+
+        return interp;
+    }
+
+    inline Extension_element tau(Extension_element sigma, Extension_element v)
+    {
+        return sigma.project().inv().lift() * v;
+    }
 
     uint64_t irred_poly(int deg);
     bool gcd1(int i, std::bitset<64> p);
