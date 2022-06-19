c53011560c5c313e8ee93141718f47950fa7c0d9
ekarpp <eetu.karppinen@aalto.fi>
2022-06-19 15:04:48 +0300

    update ext perf script

--- ext-perf.sh
+++ ext-perf.sh
@@ -5,16 +5,17 @@ cat /etc/*release
 g++ --version
 
 make clean
+make objects -j 20
 make digraph-tests
 make extension-perf
 
 make test
 
-ARGS="-s123 -t10000000"
+ARGS="-s123 -t 30"
 
-echo "32 bit"
-./extension-perf32 $ARGS
-echo "16 bit"
-./extension-perf16 $ARGS
-echo "0 bit"
-./extension-perf0 $ARGS
+for b in 0 16 32
+do
+    echo "$b bits"
+    ./extension-perf${b} $ARGS
+    ./extension-perf${b}-PAR $ARGS
+done
