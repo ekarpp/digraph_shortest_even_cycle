753896c101feb485f0082d688fb3f0b8096eb9a8
ekarpp <eetu.karppinen@aalto.fi>
2022-07-06 18:49:47 +0300

    multiple output files for processing script

--- scripts/process-algo-scale.sh
+++ scripts/process-algo-scale.sh
@@ -4,5 +4,5 @@ REPEATS=6
 graphs=("CONFIG" "COMPLETE" "CYCLE" "ERDOS")
 
 for g in ${graphs[@]}; do
-    grep -A $(($REPEATS*3)) $g $1 | grep computed > $g
+    grep -A $(($REPEATS*3)) $g $1 | grep computed > $g_$1
 done
