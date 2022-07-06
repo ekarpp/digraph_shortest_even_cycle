8c24f8e38ba015cbe582e22f07021f6d760e1e5f
ekarpp <eetu.karppinen@aalto.fi>
2022-07-06 19:05:05 +0300

    fix processing script

--- scripts/process-algo-scale.sh
+++ scripts/process-algo-scale.sh
@@ -5,5 +5,5 @@ graphs=("CONFIG" "COMPLETE" "CYCLE" "ERDOS")
 
 for g in ${graphs[@]}; do
     grep -A $(($REPEATS*3)) $g $1 | grep --line-buffered computed \
-        | awk 'NR%6 !=1 { printf ("%s %s\n", $4, $7) }' > $g_$1.plot
+        | awk 'NR%6 !=1 { printf ("%s %s\n", $4, $7) }' > ${g}_${1}.plot
 done
