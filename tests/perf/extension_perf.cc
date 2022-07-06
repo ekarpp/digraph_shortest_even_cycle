e282fd5214060dadea0b42b6b39ab9459e53f515
ekarpp <eetu.karppinen@aalto.fi>
2022-07-06 09:42:07 +0300

    fix to extension-perf

--- tests/perf/extension_perf.cc
+++ tests/perf/extension_perf.cc
@@ -51,7 +51,7 @@ int main(int argc, char **argv)
             break;
         case 't':
             exp = stoi(optarg);
-            t <<= n;
+            t <<= exp;
             break;
         }
     }
