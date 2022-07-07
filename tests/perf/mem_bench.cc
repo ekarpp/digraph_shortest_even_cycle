b921e09b4e3635c5ef31597f602450ed101a77ba
ekarpp <eetu.karppinen@aalto.fi>
2022-07-07 10:43:40 +0300

    add more tests to mem-bench

--- tests/perf/mem_bench.cc
+++ tests/perf/mem_bench.cc
@@ -3,76 +3,175 @@
 #include <vector>
 #include <omp.h>
 #include <stdint.h>
+#include <algorithm>
 
 using namespace std;
 
-// 500 mil = 4 GB (500mil * 8B = 4 bil)
+// 4 GiBs
 #define N (1ull << 29)
 #define GIBS (N*8 / (1ull << 30))
+// one line is 8*64 bits
+#define LINE 8
 
 int main(void)
 {
-    double start, end, delta;
+    double start_t, end_t, delta;
 
     vector<uint64_t> vec(N);
     uint64_t val = time(nullptr);
 
-    start = omp_get_wtime();
+    start_t = omp_get_wtime();
     for (uint64_t i = 0; i < N; i++)
         vec[i] = val;
-    end = omp_get_wtime();
+    end_t = omp_get_wtime();
 
-    delta = end - start;
+    delta = end_t - start_t;
     cout << "wrote sequentially " << GIBS
          << " GiB in " << delta << " seconds, "
          << GIBS / delta << " GiB / s." << endl;
 
     uint64_t sum = 0;
-    start = omp_get_wtime();
+    start_t = omp_get_wtime();
     for (uint64_t i = 0; i < N; i++)
         sum += vec[i];
-    end = omp_get_wtime();
+    end_t = omp_get_wtime();
     cout << sum << endl;
 
-    delta = end - start;
+    delta = end_t - start_t;
     cout << "read sequentially " << GIBS
          << " GiB in " << delta << " seconds, "
          << GIBS / delta << " GiB / s." << endl;
 
+    vector<uint64_t> idx(N);
+    for (uint64_t i = 0; i < N; i++)
+        idx[i] = i;
+
+    random_shuffle(begin(idx), end(idx));
+
+    start_t = omp_get_wtime();
+    for (uint64_t i = 0; i < N; i++)
+        sum += vec[idx[i]];
+    end_t = omp_get_wtime();
+    cout << sum << endl;
+
+    delta = end_t - start_t;
+    cout << "read sequentially random order " << GIBS
+         << " GiB in " << delta << " seconds, "
+         << GIBS / delta << " GiB / s." << endl;
+
+    vector<uint64_t> line_idx(N / LINE);
+    for (uint64_t i = 0; i < N / LINE; i++)
+        line_idx[i] = i;
+
+    random_shuffle(begin(line_idx), end(line_idx));
+
+    start_t = omp_get_wtime();
+    for (uint64_t i = 0; i < N / LINE; i++)
+    {
+        uint64_t l = line_idx[i];
+        for (uint64_t j = 0; j < LINE; j++)
+            sum += vec[l + j];
+    }
+    end_t = omp_get_wtime();
+    cout << sum << endl;
+
+    delta = end_t - start_t;
+    cout << "read sequentially random cache lines " << GIBS
+         << " GiB in " << delta << " seconds, "
+         << GIBS / delta << " GiB / s." << endl;
+
+
+    start_t = omp_get_wtime();
+    for (uint64_t i = 0; i < N; i++)
+        sum += vec[idx[i]];
+    end_t = omp_get_wtime();
+    cout << sum << endl;
+
+    delta = end_t - start_t;
+    cout << "read sequentially random cache lines " << GIBS
+         << " GiB in " << delta << " seconds, "
+         << GIBS / delta << " GiB / s." << endl;
+
+
     val = time(nullptr);
-    start = omp_get_wtime();
+    start_t = omp_get_wtime();
     #pragma omp parallel for
     for (uint64_t i = 0; i < N; i++)
         vec[i] = val;
-    end = omp_get_wtime();
+    end_t = omp_get_wtime();
 
-    delta = end - start;
+    delta = end_t - start_t;
     cout << "wrote parallelly " << GIBS
          << " GiB in " << delta << " seconds, "
          << GIBS / delta << " GiB / s." << endl;
 
-    uint64_t sums[128];
-    sum = 0;
+    uint64_t sums[omp_get_num_procs()];
     int cores = omp_get_num_procs();
     uint64_t block_size = N / cores;
-    start = omp_get_wtime();
+    start_t = omp_get_wtime();
     #pragma omp parallel for
     for (int i = 0; i < cores; i++)
     {
         uint64_t csum = 0;
-        uint64_t start = block_size * i;
-        uint64_t end = block_size * (i + 1) - 1;
-        for (uint64_t j = start; j < end; j++)
+        uint64_t first = block_size * i;
+        uint64_t last = block_size * (i + 1) - 1;
+        for (uint64_t j = first; j < last; j++)
             csum += vec[j];
         sums[i] = csum;
     }
     for (int i = 0; i < cores; i++)
         sum += sums[i];
-    end = omp_get_wtime();
+    end_t = omp_get_wtime();
     cout << sum << endl;
 
-    delta = end - start;
+    delta = end_t - start_t;
     cout << "read parallelly " << GIBS
+         << " GiB in " << delta << " seconds, "
+         << GIBS / delta << " GiB / s." << endl;
+
+    start_t = omp_get_wtime();
+    #pragma omp parallel for
+    for (int i = 0; i < cores; i++)
+    {
+        uint64_t csum = 0;
+        uint64_t first = block_size * i;
+        uint64_t last = block_size * (i + 1) - 1;
+        for (uint64_t j = first; j < last; j++)
+            csum += vec[idx[j]];
+        sums[i] = csum;
+    }
+    for (int i = 0; i < cores; i++)
+        sum += sums[i];
+    end_t = omp_get_wtime();
+    cout << sum << endl;
+
+    delta = end_t - start_t;
+    cout << "read parallelly random order " << GIBS
+         << " GiB in " << delta << " seconds, "
+         << GIBS / delta << " GiB / s." << endl;
+
+    start_t = omp_get_wtime();
+    #pragma omp parallel for
+    for (int i = 0; i < cores; i++)
+    {
+        uint64_t csum = 0;
+        uint64_t first = block_size * i / LINE;
+        uint64_t last = block_size * (i + 1) / LINE - 1;
+        for (uint64_t j = first; j < last; j++)
+        {
+            uint64_t l = line_idx[j];
+            for (uint64_t k = 0; k < LINE; k++)
+                csum += vec[l + k];
+        }
+        sums[i] = csum;
+    }
+    for (int i = 0; i < cores; i++)
+        sum += sums[i];
+    end_t = omp_get_wtime();
+    cout << sum << endl;
+
+    delta = end_t - start_t;
+    cout << "read sequentially random cache lines " << GIBS
          << " GiB in " << delta << " seconds, "
          << GIBS / delta << " GiB / s." << endl;
 
